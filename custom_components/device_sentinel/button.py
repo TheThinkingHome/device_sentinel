72299bc7564c0e9043efebf46ffcc99a55949ea7
TheThinkingHome <james_lander@xeazy.com>
2026-07-21 06:50:53 -0500

    Update button.py

--- custom_components/device_sentinel/button.py
+++ custom_components/device_sentinel/button.py
@@ -3,7 +3,7 @@
 # Device Sentinel - a Home Assistant custom integration from The Thinking Home (xeazy.com)
 #   Article: https://xeazy.com/reliable-home-assistant-dead-sensor-detection/
 #   Repository: https://github.com/TheThinkingHome/device_sentinel
-#   Version: 0.4.4 (2026-07-19)
+# File: button.py, Version: 0.4.4 (2026-07-19)
 
 """Button platform for the Device Sentinel integration.
 
